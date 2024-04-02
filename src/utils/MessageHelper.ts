f995b1d6d70db7d74d41bf24c9f21438899e3aa5
Yafet Hamouda <yhamouda@cultwinesltd.com>
2024-04-02 20:00:03 +0100

    send whatsapp message wip

---
+++ src/utils/MessageHelper.ts
@@ -0,0 +1,22 @@
+export const sendWhatsAppMessage = async () => {
+  const rawResponse = await fetch(
+    "https://graph.facebook.com/v18.0/268550596344024/messages",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer EAAUtnNZB03NsBOyrQjZAa4P4wpmRK1WYu2J0gtKwo6vbZCEVj9YFQmAEw5ZBBsNIrhdWDKLZBItw5GotikPHkCmIkNkI6aXjVY4ZCZAh8oleZBEr5r3nDLbfIkkUf6A7nixi6k5Pq4AVEc9ven8cSAsTtK0ZAECOziamloHKKrjANGUcs7tGDlhDrsQh5MLZBdZAZCLs8CiA2pljxIqFy3V6",
+      },
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: "21694300287",
+        type: "template",
+        template: { name: "hello_world", language: { code: "en_US" } },
+      }),
+    }
+  );
+  const content = await rawResponse.json();
+  console.log(content);
+};
