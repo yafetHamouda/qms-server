295269d8ba896892aa2f6a5a3dae6d175753a26b
Yafet Hamouda <yhamouda@cultwinesltd.com>
2024-04-04 20:13:48 +0100

    added logic to ignore tickets process duration with more than 45 minutes

--- src/middlewares/storeProcessedTime.ts
+++ src/middlewares/storeProcessedTime.ts
@@ -1,6 +1,8 @@
 import { Response, Request, NextFunction } from "express";
 import TicketProcess from "../models/TicketProcess.js";
 
+const FORTY_FIVE_MINUTES_MS = 2700000;
+
 export default async function (
   req: Request,
   res: Response,
@@ -25,6 +27,12 @@ export default async function (
       const processDurationMS =
         new Date().getTime() -
         previouslyProcessedTicketByWindow.createdAt.getTime();
+
+      // if a ticket process duration exceeds 45 minutes, for now, we consider that as polluted data so it does not affect the avg
+      // calculation time
+      if (processDurationMS > FORTY_FIVE_MINUTES_MS) {
+        return next();
+      }
 
       await TicketProcess.findByIdAndUpdate(
         previouslyProcessedTicketByWindow.id,
